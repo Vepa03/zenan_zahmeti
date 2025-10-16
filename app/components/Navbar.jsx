74f6b28689ec3b1cbef1f99a68f1da3f176552b9
Vepa Babayev <vepababayev03@gmail.com>
2025-10-17 00:28:34 +0200

    added buttons

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -5,15 +5,14 @@ const Navbar = () => {
     <>
     <nav className='w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between'>
         <a href='/'>Zenan</a>
-        <ul className='hidden md:flex items-center gap-6 rounded-full px-12 py-3 border border-gray-500'>
-           <li><a href='/home'>Home</a></li> 
-           <li><a href='/product'>Product</a></li> 
-           <li><a href='/bag'></a>Bag</li> 
-           <li><a href='/contact'></a>Contact</li> 
+        <ul className='hidden md:flex items-center gap-6 rounded-full px-12 py-3 
+        bg-white shadow-sm bg-opacity-50'>
+           <li><a href='/home' className='font-Ovo'>Home</a></li> 
+           <li><a href='/product' className='font-Ovo'>Product</a></li> 
+           <li><a href='/bag' className='font-Ovo'>Bag</a></li> 
+           <li><a href='/contact' className='font-Ovo'>Contact</a></li> 
         </ul>
-        <div className='rounded-full border px-6 py-3 border-gray-500'>
-            <a href='/login'>Login</a>
-        </div>
+        <button href='/login' className='bg-green-600 rounded-full px-6 py-3 text-white hover:bg-green-700 font-bold '>Login</button>
     </nav>
     </>
   )
